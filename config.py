0225cef582d939707e7c1d05acd54aadb32b5dbb
CHOI HWAN IL <hichoe95@naver.com>
2021-12-30 15:42:12 +0900

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def parse_args():
     parser.add_argument("--log_dir", type=str, default = 'runs', help="dir for tensorboard")
     parser.add_argument("--image_name", type = str, default = 'gen_images', help="")
 
-    parser.add_argument("--iter_num", type=int, default=100000, help="number of iterations of training")
+    parser.add_argument("--iter_num", type=int, default=200000, help="number of iterations of training")
     parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
     parser.add_argument("--latent_dim", type = int, default=128, help="dimension of latent vector")
 
@@ -22,6 +22,7 @@ def parse_args():
     parser.add_argument("--lr", type=float, default=0.0001, help="learning rate")
     parser.add_argument("--lambda_gp", type=float, default=10, help="amount of gradient penalty loss")
     # parser.add_argument("--weight_init", type=bool, default=False, help="conv weight init from normal dist")
+    parser.add_argument("--loss", type=str, default='wgangp', help="wgangp or bce, default is wgangp")
     parser.add_argument("--optim", type=str, default='Adam', help="choose only Adam or RMSprop")
     parser.add_argument("--generator_upsample", type=bool, default=False, help="if False, using ConvTranspose.")
     parser.add_argument("--normalization", type=str, default='None', help="inorm : instancenorm, bnorm : batchnorm, or None")

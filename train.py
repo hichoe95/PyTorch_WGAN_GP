bcf5d95b18039ed0f45803d0150b4e8475740974
CHOI HWAN IL <hichoe95@naver.com>
2021-12-30 15:41:32 +0900

    Update train.py

--- train.py
+++ train.py
@@ -111,9 +111,7 @@ def train(G, D, optim_G, optim_D, dataset, configs):#iter_num = 100, batch_size 
         with torch.no_grad():
 
             if i % 100 == 0:
-                print(f'iter{i:d}')
                 gs = gridspec.GridSpec(1, latent_fixed.size(0), wspace = 0.02, hspace = 0.1)
-                plt.tight_layout()
                 plt.figure(figsize = (3*latent_fixed.size(0), 3))
                 
                 imgs = G(latent_fixed.to(device))
@@ -122,6 +120,7 @@ def train(G, D, optim_G, optim_D, dataset, configs):#iter_num = 100, batch_size 
                     plt.subplot(gs[0,j])
                     plt.imshow(minmax(imgs[j].detach().cpu()).permute(1,2,0))
                     plt.axis('off')
+                    plt.tight_layout()
                     
                 plt.savefig(configs.image_name)
                 
@@ -142,3 +141,123 @@ def train(G, D, optim_G, optim_D, dataset, configs):#iter_num = 100, batch_size 
             for p_G, p_D in zip(optim_G.param_groups, optim_D.param_groups):
                 p_G['lr'] = g_lr
                 p_D['lr'] = d_lr
+
+
+def train_bce(G, D, optim_G, optim_D, dataset, configs):#iter_num = 100, batch_size = 30):
+    
+    if configs.use_tensorboard:
+        from tensorboardX import SummaryWriter
+
+        if not os.path.exists(configs.log_dir):
+            os.mkdir(configs.log_dir)
+        summary = SummaryWriter(logdir = configs.log_dir)
+
+    
+    device = f'cuda:{configs.main_gpu:d}' if torch.cuda.is_available() else 'cpu'
+    
+    if torch.cuda.is_available():
+        G.to(device)
+        D.to(device)
+
+        latent_fixed = torch.randn((3, configs.latent_dim)).to(device)
+    
+    data_iter = iter(dataset)
+    
+    g_lr = configs.lr
+    d_lr = configs.lr
+    
+    adversarial_loss = torch.nn.BCEloss()
+        
+    for i in tqdm(range(configs.iter_num)):
+        
+        reals = next(data_iter)
+
+        if torch.cuda.is_available():
+            reals = reals[0].to(device)
+            latents = torch.randn((configs.batch_size, configs.latent_dim)).to(device)
+        
+        # discriminator
+        
+        # for real
+        d_real_out = D(reals)
+        loss_real = adversarial_loss(d_real_out, torch.tensor((configs.batch_size, 1)).fill_(1.).requires_grad_(False))
+        
+        # for fake
+        fake_img = G(latents)
+        d_fake_out = D(fake_img.detach())
+        loss_fake = adversarial_loss(d_fake_out, torch.tensor((configs.batch_size, 1)).fill_(0.).requires_grad_(False))
+        
+        d_loss = (loss_real + loss_fake)/2
+        
+        G.zero_grad()
+        D.zero_grad()
+        d_loss.backward()
+        optim_D.step()
+        
+        loss = {}
+            
+        # log for tensorboard
+        summary.add_scalar('D/loss_real', loss_real.item(), i)
+        summary.add_scalar('D/loss_fake', loss_fake.item(), i)
+        summary.add_scalar('D/d_loss', d_loss.item(), i)
+
+        loss['D/loss_real'] = loss_real.item()
+        loss['D/loss_fake'] = loss_fake.item()
+        loss['D/d_loss'] = d_loss.item()
+        
+        # generator
+            
+        latents = torch.randn((configs.batch_size, configs.latent_dim)).to(device)
+
+        gen_out = G(latents)
+        d_out = D(gen_out)
+
+        g_loss = adversarial_loss(d_out, torch.tensor((configs.batch_size, 1)).fill_(1.).requires_grad_(False))
+
+        G.zero_grad()
+        g_loss.backward()
+        optim_G.step()
+        
+        # log for tensorboard
+        summary.add_scalar('G/loss_fake', loss_fake.item(), i)
+        
+        loss['G/loss_fake'] = g_loss.item()
+        
+        # misc
+        with torch.no_grad():
+
+            if i % 100 == 0:
+                gs = gridspec.GridSpec(1, latent_fixed.size(0), wspace = 0.02, hspace = 0.1)
+                plt.figure(figsize = (3*latent_fixed.size(0), 3))
+                
+                imgs = G(latent_fixed.to(device))
+                
+                for j in range(latent_fixed.size(0)):
+                    plt.subplot(gs[0,j])
+                    plt.imshow(minmax(imgs[j].detach().cpu()).permute(1,2,0))
+                    plt.axis('off')
+                    plt.tight_layout()
+
+                    
+                plt.savefig(configs.image_name)
+                
+                # print(f'D/d_loss: {loss['D/d_loss']:.2f}, D/loss_real: {loss['D/loss_real']:.2f}, D/loss_fake: {loss['D/loss_fake']:.2f}, G/loss_fake: {loss['G/loss_fake']:.2f}')
+
+        # Save model checkpoints.
+        if not os.path.exists(f'./checkpoint{configs.main_gpu:d}'):
+            os.mkdir(f'./checkpoint{configs.main_gpu:d}')
+        if (i+1) % 10000 == 0:
+            G_path = os.path.join(f'./checkpoint{configs.main_gpu:d}', '{}-G.ckpt'.format(i+1))
+            D_path = os.path.join(f'./checkpoint{configs.main_gpu:d}', '{}-D.ckpt'.format(i+1))
+
+        # learning rate decay
+        
+        if i >= 10000 and i % 1000 == 0:
+            g_lr -= (g_lr / float(100000))
+            d_lr -= (d_lr / float(100000))
+            for p_G, p_D in zip(optim_G.param_groups, optim_D.param_groups):
+                p_G['lr'] = g_lr
+                p_D['lr'] = d_lr
+
+
+

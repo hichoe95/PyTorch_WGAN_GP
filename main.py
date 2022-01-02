3aafb84ba49d5191d5bc8975c5111331ab14a724
CHOI HWAN IL <hichoe95@naver.com>
2022-01-02 10:13:24 +0900

    2022/01/02

--- main.py
+++ main.py
@@ -2,20 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import torch
-import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.nn as nn
-import torch.nn.functional as F
 import os.path
-import torch.nn.parallel
 import torch.utils.data as data
 import torchvision.datasets as datasets
-import torchvision.models as models
 import random
-from matplotlib import gridspec
 from tqdm import tqdm
-
 
 from config import *
 from misc import *
@@ -29,12 +23,7 @@ def main(args):
 
 	train_data = train_loader(configs = args)
 
-	# if args.generator_upsample:
-	# 	G = Generator_up(configs = args)
-	# else:
-	# 	G = Generator(configs = args)
-
-	G = Generator_up(configs = args)
+	G = Generator(configs = args)
 	D = Discriminator(configs = args)
 
 	print(f'Ratio of G and D params : {get_n_params(G)/get_n_params(D):f}')
@@ -45,8 +34,8 @@ def main(args):
 		print('Weights of both G and D are initiated.')
 
 	if args.optim == 'Adam':
-		optim_D = optim.Adam(D.parameters(), lr=args.lr, betas=(0.5, 0.999))
-		optim_G = optim.Adam(G.parameters(), lr=args.lr, betas=(0.5, 0.999))
+		optim_D = optim.Adam(D.parameters(), lr=args.lr, betas=args.betas)
+		optim_G = optim.Adam(G.parameters(), lr=args.lr, betas=args.betas)
 	elif args.optim == 'RMSprop':
 		optim_D = optim.RMSprop(D.parameters(), lr=args.lr)
 		optim_G = optim.RMSprop(G.parameters(), lr=args.lr)
